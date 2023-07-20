f38f89c1743c4860607aa9230725019df3bc18e9
Ameya Padole <42582133+ameyapadole@users.noreply.github.com>
2023-07-20 01:42:17 -0700

    FirstCommit

---
+++ java-edge/src/main/java/integrations/turnitin/com/membersearcher/service/MembershipService.java
@@ -0,0 +1,44 @@
+package integrations.turnitin.com.membersearcher.service;
+
+import java.util.concurrent.CompletableFuture;
+
+import integrations.turnitin.com.membersearcher.client.MembershipBackendClient;
+import integrations.turnitin.com.membersearcher.model.MembershipList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class MembershipService {
+	@Autowired
+	private MembershipBackendClient membershipBackendClient;
+
+	/**
+	 * Method to fetch all memberships with their associated user details included.
+	 * This method calls out to the php-backend service and fetches all memberships,
+	 * it then calls to fetch the user details for each user individually and
+	 * associates them with their corresponding membership.
+	 *
+	 * @return A CompletableFuture containing a fully populated MembershipList object.
+	 * 
+	 * Here the code now users fetchUsers() method to fetch all users in a single API call.
+	 * We then populate the list of users in the MembershipList object.
+	 * 
+	 */
+
+	public CompletableFuture<MembershipList> fetchAllMembershipsWithUsers() {
+		return membershipBackendClient.fetchMemberships()
+				.thenCompose(users -> membershipBackendClient.fetchUsers()
+							.thenApply(userList -> {
+
+								for (int i = 0; i < users.getMemberships().size(); i++) {
+
+									users.getMemberships().get(i).setUser(userList.getUsers().get(i));
+								}
+								return users;
+							}));
+						}
+}
+
+	
